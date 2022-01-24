7bc82ce85c80bed96e2ce98d1557b5b4288d052b
Paulo Magalhães <pmagalhaesr@outlook.com>
2022-01-24 00:16:23 -0300

    Criei page

--- index.php
+++ index.php
@@ -2,15 +2,17 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use Slim\Slim;
+use \Hcode\page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$sql = new Hcode\DB\Sql();
-	$results = $sql->select("SELECT * FROM tb_users");
-	echo json_encode($results);
+	$page = new page();
+	$page->setTpl("index");
 	
 });
 
